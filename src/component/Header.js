5f522c7ab63f3a7fb54427d2ebdbc7d2bf5b85b5
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-06-16 03:12:20 +0600

    everything done except styling

---
+++ src/component/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar,Container,Nav } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg"  variant="light">
+  <Container>
+  <Navbar.Brand href="#home">ATools<span className='text-danger'>.</span></Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#features"></Nav.Link>
+      <Nav.Link href="#pricing"></Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link style={{backgroundColor: '#03203C'}} className='text-light fw-bold me-3 py-2 px-3' href="#freetrials">Start Free Trial</Nav.Link>
+      <Nav.Link style={{backgroundColor: '#E07C24'}} className='text-light fw-bold py-2 px-3' eventKey={2} href="#login">
+        Login
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    );
+};
+
+export default Header;
---
+++ src/component/Login/Login.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "./Login.css";
+
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const data = { email, password };
+    const url = `http://localhost:5000/login`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+    event.target.reset();
+  };
+  return (
+    <div className="container">
+      <div className="d-flex align-items-center justify-content-center mt-5">
+        <div className="container w-50 mb-4">
+          <h2 className="text-secondary text-center my-5">
+            {" "}
+            <span className="fw-bolder text-dark">Welcome Back</span>{" "}
+          </h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group
+              className="mb-3  mx-auto small-view"
+              controlId="formBasicEmail"
+            >
+              <Form.Control
+                name="email"
+                type="email"
+                placeholder="Email Address"
+                required
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3 mx-auto small-view"
+              controlId="formBasicPassword"
+            >
+              <Form.Control
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+
+            <Button
+              className="fw-bold mx-auto d-block small-view mb-1"
+              style={{ backgroundColor: "#03203C" }}
+              type="submit"
+            >
+              Login
+            </Button>
+          </Form>
+          <div className="d-flex justify-content-around mt-2">
+            <div className="size fw-bold">
+              <input
+                // onClick={() => setAgree(!agree)}
+                type="checkbox"
+                name="terms"
+                id="terms"
+              />
+              <label className="" htmlFor="terms">
+                Remember Password
+              </label>
+            </div>
+            <div className="size fw-bold">
+              <p>Forget Password?</p>
+            </div>
+          </div>
+        </div>
+        <div className="d-none  d-lg-block">
+          <img src="https://i.ibb.co/tDZLWJV/login1.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
